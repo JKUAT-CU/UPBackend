e473292be7a91a4529d8a060823ded49bc255162
Odingoii <okoromaloba@gmail.com>
2025-03-10 17:48:29 +0300

    Initial Commit

--- registration.php
+++ registration.php
@@ -1,7 +1,7 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
 
 // Include database connection
 include 'backend/db.php'; // Ensure this file contains the correct database connection code
