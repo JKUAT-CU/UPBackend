0cae545fc8968085ba3f74d639d1077fd2b3d7c9
Odingoii <okoromaloba@gmail.com>
2025-03-10 17:00:35 +0300

    Initial Commit

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 // Perform the redirection to mission.php
-header("Location: mission.php");
+header("Location: registration.php");
 exit(); // Make sure no further code is executed after the redirection
 ?>
